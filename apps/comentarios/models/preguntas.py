07e80945d244314cd521480bb906587bbfc410e1
Froylan Camacho Tellez <Famachot@hotmail.com>
2013-09-25 16:22:49 -0500

    se agrega parte del módulo para hacer las preguntas.

--- apps/comentarios/models/preguntas.py
+++ apps/comentarios/models/preguntas.py
@@ -5,12 +5,14 @@ from apps.promociones.models.promocion import Promocion
 
 from django.utils.translation import ugettext_lazy as _
 
+from apps.empresas.models.empresa import Categoria
+
 
 
 
 class Preguntas_promocion(models.Model):
-	#promocion = models.ForeignKey(Promocion)
 	pregunta = models.CharField(max_length=45,verbose_name=(u'Pregunta'))
+	categoria = models.ForeignKey(Categoria,null=True)
 	class Meta:
 		verbose_name = _('Pregunta de la promocion')
 		verbose_name_plural = _('Preguntas para calificar las promociones')
