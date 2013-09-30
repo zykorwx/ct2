be51d8c58a5bfe6fcf1390534c07b26dc0d6513f
zykor <enrique.wx@gmail.com>
2013-09-30 11:59:33 -0500

    cambiando a python-social-auth

--- configuracion/settings.py
+++ configuracion/settings.py
@@ -24,7 +24,7 @@ DATABASES = {
         'NAME': 'db_clicktotal',                      # Or path to database file if using sqlite3.
         'USER': 'viewor',                      # Not used with sqlite3.
         'PASSWORD': 'viewor_12',                  # Not used with sqlite3.
-        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
+        'HOST': '198.199.120.36',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '3306',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
